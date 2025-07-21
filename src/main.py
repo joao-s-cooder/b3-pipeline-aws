de5b9ec1bc230325f1e5fe54fde140b44afc06bf
Joao Silva <ajoao@ymail.com>
2025-07-21 16:53:50 -0300

    Atualizando Script para Lambda

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.core.data_processor import DataProcessor
 from src.utils.s3_uploader import S3UpLoader
 from src.config.s3_config import S3_BUCKET_NAME, S3_REGION_NAME, S3_UPLOAD_PREFIX_B3, S3_UPLOAD_PREFIX_BITCOIN
 import pandas as pd
+import json
 import os
 from datetime import datetime
 
@@ -24,15 +25,18 @@ def run_b3_extractor():
         # Gerar o nome do arquivo
         timestamp = pd.Timestamp.now().strftime('%Y%m%d_%H%M%S')
         file_name = f"b3_portifoliio_{timestamp}.parquet"
-        output_path = os.path.join("data", file_name)
+        output_path = os.path.join("/tmp", file_name)
 
-        os.makedirs('data', exist_ok=True)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
         df_b3_cleaned.to_parquet(output_path, index=False, compression='snappy')
         print(f"Dados da B3 extraidos e salvos em '{output_path}'")
 
         # Enviar para o S3
         s3_object_name = f"{S3_UPLOAD_PREFIX_B3}{file_name}"
         s3_uploader.upload_file(output_path, s3_object_name)
+
+        os.remove(output_path)
+        print(f"Arquivo temporário '{output_path}' removido.")
     else:
         print("Nenhum dado coletado da B3 ou ocorreu erro.")
 
@@ -51,20 +55,41 @@ def run_bitcoin_extractor(minutes: int = 1):
         #Gerar o nome do arquivo
         timestamp = pd.Timestamp.now().strftime('%Y%m%d_%H%M%S')
         file_name = f"bitcoin_data_{timestamp}.parquet"
-        output_path = os.path.join("data", file_name)
+        output_path = os.path.join("/tmp", file_name)
 
-        os.makedirs('data', exist_ok=True)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
         df_bitcoin_cleaned.to_parquet(output_path, index=False, compression='snappy')
         print(f"Dados do Bitcoin limpos e salvos em '{output_path}'")
 
         # Enviar para o S3
         s3_object_name = f"{S3_UPLOAD_PREFIX_BITCOIN}{file_name}"
         s3_uploader.upload_file(output_path, s3_object_name)
+
+        os.remove(output_path)
+        print(f"Arquivo temporário '{output_path}' removido.")
     else:
         print("Nenhum dado coletado do Bitcoin ou ocorreu erro.")
 
 
-
-if __name__ == "__main__":
-    run_b3_extractor()
-    run_bitcoin_extractor(minutes=3)
+# --- Handler para AWS Lambda ---
+def lambda_handler(event, context):
+    """
+    Função principal que será executada pelo AWS Lambda.
+    """
+    print("Iniciando execução do scraper via Lambda...")
+    
+    try:
+        run_b3_extractor()
+        run_bitcoin_extractor(minutes=3) 
+        
+        print("Execução do scraper concluída com sucesso.")
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Scraping e upload concluídos com sucesso!')
+        }
+    except Exception as e:
+        print(f"Erro durante a execução do scraper: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Erro na execução: {str(e)}')
+        }
